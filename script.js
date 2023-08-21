def0cdc8ca3a958042a907ca55298e0e21a696f0
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-21 15:37:39 +0530

    for loop

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ for (let i = 0 ; i< 5; i++){
     text += "hello" + "<br>"
     document.getElementById("para").innerHTML=text
 }
+
+
+const cars =['audi', 'bmw', 'scorpio'];
+let i, len, text1;
+for (i =0 ,len = cars.length, text="";i < len ; i++){
+    text1 += cars[i] + "<br>"
+    document.getElementById("para1").innerHTML=text1;
+
+}
